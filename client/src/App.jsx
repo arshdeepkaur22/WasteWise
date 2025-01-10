ebdea19f346d5bed2149f733be728a91357b1a17
arshdeepkaur22 <arshd33p.kaur05@gmail.com>
2025-01-10 13:38:03 +0530

    dustbin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,27 @@
 
-import { BrowserRouter as  Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css'
 import Navbar from "./components/Navbar"
-import Events from './components/Events';
+
 import Dashboard from './components/Dashboard'
+import Events from './components/Events';
 
 
 function App() {
   
-
   return (
-  
-    <div className="flex flex-col min-h-screen">
-      
+    <>
+      <div className="flex flex-col min-h-screen">
       <Navbar />
-
-    
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/events" element={<Events />} />
+      <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/events" element={<Events />} />
           
-        </Routes>
-      </main>
+          </Routes>
     </div>
-  
+    </>
   )
 }
 
